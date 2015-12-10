ee0a4c1a42d73318dcd7eed65d4c263235053740
Janderson de Melo Antunes <janderson.antunes@trt11.jus.br>
2015-12-10 15:00:36 -0400

    added json file and call ajax to fetch comments

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,11 +5,27 @@ var data = [
 
 
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data}/>
+        <CommentList data={this.state.data}/>
         <CommentForm/>
       </div>
     );
@@ -64,6 +80,6 @@ var Comment = React.createClass({
 
 
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url="/api/comments" />,
   document.getElementById('content')
 );
