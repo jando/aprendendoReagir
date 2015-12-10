4444dbd754386582235fff42c201f01c22fca440
Janderson de Melo Antunes <janderson.antunes@trt11.jus.br>
2015-12-10 14:05:25 -0400

    Added comments

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,6 +2,7 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
+        <h1>Comments</h1>
         <CommentList/>
         <CommentForm/>
       </div>
@@ -13,7 +14,8 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
       </div>
     );
   }
@@ -24,6 +26,20 @@ var CommentForm = React.createClass({
     return (
       <div className="commentForm">
         Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+// tutorial4.js
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
       </div>
     );
   }
